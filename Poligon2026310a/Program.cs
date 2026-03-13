d816391b3a490c1aa7e874498d20dbbf1ee03cff
Ucenik <Ucenik@DESKTOP-USO4G6P>
2026-03-13 17:34:11 +0100

    Add project files.

---
+++ Poligon2026310a/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poligon2026310a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dusan Kovacevic
+        }
+    }
+}
